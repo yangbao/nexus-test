d90c58dcb8f44357c18000d602432f0adaf15474
paul.yang <byang1@idexcorp.com>
2022-08-16 13:47:45 +0800

    init

---
+++ src/test/java/com/example/nexustest/NexusTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexustest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
